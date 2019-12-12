7829221c2c6704053fbf71a7de8732654564ee54
AmoleR <43121460+AmoleR@users.noreply.github.com>
2019-12-11 22:03:26 -0800

    Updates

---
+++ precache-manifest.7ba3df4a579427bac941a8ea214e3d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0efcc1d2e7cc32341898e7b668dee0ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c67f747d38cc12ed51",
+    "url": "/static/css/main.c2befbbc.chunk.css"
+  },
+  {
+    "revision": "eb97102aae30d0634806",
+    "url": "/static/js/2.533937a1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.533937a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8c67f747d38cc12ed51",
+    "url": "/static/js/main.43d8cfd4.chunk.js"
+  },
+  {
+    "revision": "322d8a0c301fc0739ee2",
+    "url": "/static/js/runtime-main.4f9a2789.js"
+  }
+]);
