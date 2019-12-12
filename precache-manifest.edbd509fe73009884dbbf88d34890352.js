6d8e07c876974d29c30c411d05fbf07f18629a79
AmoleR <43121460+AmoleR@users.noreply.github.com>
2019-12-11 22:05:52 -0800

    Updates

---
+++ precache-manifest.edbd509fe73009884dbbf88d34890352.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec40c9fe809b736f30b374b2b0da524c",
+    "url": "/biology/index.html"
+  },
+  {
+    "revision": "241ade56f6b2ce6b96e4",
+    "url": "/biology/static/css/main.c2befbbc.chunk.css"
+  },
+  {
+    "revision": "eb97102aae30d0634806",
+    "url": "/biology/static/js/2.533937a1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/biology/static/js/2.533937a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "241ade56f6b2ce6b96e4",
+    "url": "/biology/static/js/main.21ce21c5.chunk.js"
+  },
+  {
+    "revision": "f97dd5894ecf0656f455",
+    "url": "/biology/static/js/runtime-main.1b66ef54.js"
+  }
+]);
